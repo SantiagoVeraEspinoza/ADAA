9eb938ff1f11f555846ac9fce2b82ced29322991
Santiago Vera Espinoza <santiveraespinoza@gmail.com>
2023-11-09 18:58:38 -0600

    Finished activity 4.4

--- Clase/Actividad_4.4/main.cpp
+++ Clase/Actividad_4.4/main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <vector>
+#include <stdlib.h>
+#include <time.h>
 
 using namespace std;
 
@@ -39,15 +41,65 @@ std::ostream& operator<<(std::ostream& os, const std::pair<T1, T2>& p) {
     return os;
 }
 
+// Enfoque iterativo para buscar un elemento - O(n)
+pair <int, int> iterativeRandBinarySerach(vector <int> input, int target) {
+    int l = 0;
+    int r = input.size() - 1;
+
+    int p; // Puntero aleatorizado
+    int it = 0; // Contador de iteraciones
+
+    // En el peor de los casos se probarán todos los elementos del arrreglo
+    while (l <= r && input[p] != target) { // Mientras exista espacio entre los límites y no se haya encontrado el target - O(n)
+        it++; // Incrementa el contador
+        p = l + rand() % (r - l + 1); // Elige un numero aleatorio en el rango
+
+        if (target < input[p]) { r = p - 1; continue; } // Es mayor, se recorre la derecha
+        if (target > input[p]) { l = p + 1; continue; } // Es menor, se recorre a la izquierda
+
+        break;
+    }
+
+    if (input[p] != target) return {-1, it}; // Si no encuentra el elemento
+
+    return {p, it};
+}
+
+// Enfoque recursivo para buscar un elemento - O(n)
+pair <int, int> recursiveRandBinarySearch(vector <int> input, int target, int l=0, int r=-10) {
+    if (r == -10) r = input.size() - 1; // Establece el puntero derecho si se pasa el valor por defecto
+
+    pair <int, int> res = {-1, 0}; // Valor dado un error
+
+    if (l > r) return res; // Caso límite donde no hay solución, limita la complejidad
+
+    int p = l + rand() % (r - l + 1); // Elige un numero aleatorio en el rango
+
+    if (target == input[p]) { // Se encuentra el objetivo
+        res = make_pair(p, 0);
+    }
+    else if (target < input[p]) { // El objetivo es menor
+        res = recursiveRandBinarySearch(input, target, l, p - 1); // Recursión, se puede ejecutar un máximo de n veces antes de llegar al caso límite - O(n)
+    }
+    else if (target > input[p]) { // El objetivo es mayor
+        res = recursiveRandBinarySearch(input, target, p + 1, r); // Recursión, se puede ejecutar un máximo de n veces antes de llegar al caso límite - O(n) 
+    }
+
+    return make_pair(res.first, res.second + 1);
+}
+
+// Método principal - O(3n) -> O(n)
 void process()
 {
+    srand(time(NULL));
+
     int n, f;
 
     cin >> n;
 
     vector<int> input;
     
-    for (int i=0; i<n; i++) {
+    for (int i=0; i<n; i++) { // Lee todos los elementos - O(n)
         int e;
         cin >> e;
         input.push_back(e);
@@ -55,7 +107,23 @@ void process()
 
     cin >> f;
 
-    cout << input;
+    pair <int, int> res_it = iterativeRandBinarySerach(input, f); // Se obtiene el resultado por iteración - O(n)
+    pair <int, int> res_re = recursiveRandBinarySearch(input, f); // Se obtiene el resultado por recursión - O(n)
+
+    if (res_it.first != -1 && res_re.first != -1) { // Encontró el elemento
+        cout << "Version iterativa: Se evaluaron " << res_it.second << " iteraciones, el elemento " << f 
+             << " está en la posicion " << res_it.first << "." << endl;
+
+        cout << "Version recursiva: Se evaluaron " << res_re.second << " iteraciones, el elemento " << f 
+             << " está en la posicion " << res_re.first << "." << endl;
+
+        return;
+    }
+
+    // No encontró el elemento
+    cout << "Version iterativa: Se evaluaron " << res_it.second << " iteraciones, no se encontró el elemento " << f << endl;
+
+    cout << "Version recursiva: Se evaluaron " << res_re.second << " iteraciones, no se encontró el elemento " << f << endl;
 }
 
 #ifdef _WIN32
